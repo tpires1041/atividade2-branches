f3f7c7f9748858bb7a8793edcaf734e98ff44549
Tiago Pires <tiagopires2501@outlook.com>
2024-10-26 21:39:25 -0400

    Update script.js
    Adicionado getElementById

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let mostrarDados = function()
 {
   let nome = document.getElementsByClassName("nome").value
-  let idade = document("idade").value
+  let idade = document.getElementById("idade").value
   let ano = document.getElementById("ano").value
 
     document.getElementById("resultadoNomes").innerHTML = nome
