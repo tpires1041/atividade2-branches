2294ad9eb69359e70ea2faa5301ecd86164755a6
Tiago Pires <tiagopires2501@outlook.com>
2024-10-26 21:36:38 -0400

    commit inicial

---
+++ script.js
@@ -0,0 +1,10 @@
+let mostrarDados = function()
+{
+  let nome = document.getElementsByClassName("nome").value
+  let idade = document("idade").value
+  let ano = document.getElementById("ano").value
+
+    document.getElementById("resultadoNomes").innerHTML = nome
+    document.getElementById("resultadoIdade").innerHTML = idade
+    document.getElementById("resultadoAno").innerHTML = ano
+  }
