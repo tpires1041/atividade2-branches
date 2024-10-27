ac01bdd87392f98b334a856c1cdd2700efa43733
Tiago Pires <tiagopires2501@outlook.com>
2024-10-26 21:42:25 -0400

    Update script.js
    Alteração de nomes para nome

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let mostrarDados = function()
   let idade = document("idade").value
   let ano = document.getElementById("ano").value
 
-    document.getElementById("resultadoNomes").innerHTML = nome
+    document.getElementById("resultadoNome").innerHTML = nome
     document.getElementById("resultadoIdade").innerHTML = idade
     document.getElementById("resultadoAno").innerHTML = ano
   }
